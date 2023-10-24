b8d9c8ea8e7cce85b0715018370ce1b6b83e3331
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-24 20:57:01 +0000

    Add filters to admin panel classes. Create test posts and categories

--- cookingblog/admin.py
+++ cookingblog/admin.py
@@ -9,6 +9,10 @@ class CookingRecipePostAdmin(SummernoteModelAdmin):
     """
     summernote_fields = ('cooking_recipe_body')
     list_display=('cooking_recipe_title', 'slug', 'status')
+    prepopulated_fields = {'slug': ('cooking_recipe_title',)}
+    search_fields = ['cooking_recipe_title', 'cooking_recipe_body']
+    list_filter = ('status', 'created_on')
+
     
 
 
@@ -19,6 +23,9 @@ class CommentAdmin(admin.ModelAdmin):
     Registration Comment model in admin, add filters and displaying fields
     """
     list_display = ('name', 'body', 'cooking_recipe_post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
     
 
     def approve_comments(self, request, queryset):
@@ -28,7 +35,7 @@ class CommentAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     """
-    Registration Category model in admin, add filters and displaying fields
+    Registration Category model in admin and displaying fields
     """
     list_display = ('name', 'slug', 'id')
     prepopulated_fields = {"slug": ("name", )}

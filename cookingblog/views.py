807e836cef4aa0fa52378031fecfcaf4e9d39f97
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-24 20:59:00 +0000

    Add add view for displaying posts list

--- cookingblog/views.py
+++ cookingblog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import CookingRecipePost
 
-# Create your views here.
+
+class CookingRecipesPostList(generic.ListView):
+    """
+    View for displaying a list of posts
+    """
+    model = CookingRecipePost
+    queryset = CookingRecipePost.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 5
+    context_object_name = 'cooking_recipes_post_list'

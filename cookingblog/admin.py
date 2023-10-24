abf4c5e9dd05688821bd3ca2aadfb79967e68c60
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-24 20:50:29 +0000

    Add classes for models in admin

--- cookingblog/admin.py
+++ cookingblog/admin.py
@@ -1,4 +1,36 @@
 from django.contrib import admin
 from .models import CookingRecipePost, Comment, Category
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(CookingRecipePost)
+@admin.register(CookingRecipePost)
+class CookingRecipePostAdmin(SummernoteModelAdmin):
+    """
+    Registration CookingRecipePost model in admin, add filters and displaying fields
+    """
+    summernote_fields = ('cooking_recipe_body')
+    list_display=('cooking_recipe_title', 'slug', 'status')
+    
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Registration Comment model in admin, add filters and displaying fields
+    """
+    list_display = ('name', 'body', 'cooking_recipe_post', 'created_on', 'approved')
+    
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Registration Category model in admin, add filters and displaying fields
+    """
+    list_display = ('name', 'slug', 'id')
+    prepopulated_fields = {"slug": ("name", )}
+
+

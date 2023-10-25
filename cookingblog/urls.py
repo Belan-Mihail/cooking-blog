31e3c9a19d6b71b7ffa382fc234958240789b4cd
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-25 06:15:44 +0000

    Add path to read single posts< update links in index.htlm

--- cookingblog/urls.py
+++ cookingblog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.CookingRecipesPostList.as_view(), name="home"),
+    path('<slug:slug>/', views.CookingRecipePostDetail.as_view(), name='cooking_recipe_post_detail'),
 ]

968d57701f67f7b1136b2b29e9c34f462de5b9cb
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-26 13:41:44 +0000

    Add class for for update posts on the site

--- cookingblog/forms.py
+++ cookingblog/forms.py
@@ -14,8 +14,17 @@ class CommentForm(forms.ModelForm):
 
 class CookingRecipePostCreateForm(forms.ModelForm):
     """
-    Form for posting posts  to the site
+    Form for posting posts on the site
     """
     class Meta:
         model = CookingRecipePost
         fields = ('cooking_recipe_title', 'cat', 'cooking_recipe_image', 'excerpt', 'cooking_recipe_body')
+
+
+class CookingRecipePostUpdateForm(forms.ModelForm):
+    """
+    Form for update posts on the site
+    """
+    class Meta:
+        model = CookingRecipePost
+        fields = ('cooking_recipe_title', 'cat', 'cooking_recipe_image', 'cooking_excerpt', 'cooking_recipe_body')

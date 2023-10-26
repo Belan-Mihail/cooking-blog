4560a89aa86008fbba9d848dfb1a07b2dda00ece
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-26 04:21:31 +0000

    Install allauth and add it to settings.py and urls.py

--- cookingproject/urls.py
+++ cookingproject/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("cookingblog.urls"), name="cookingblog-urls"),
+    path("accounts/", include("allauth.urls")),
 ]

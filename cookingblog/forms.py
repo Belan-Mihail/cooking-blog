04c7792745520bf970814f836c6217b68b8d6d1f
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-26 12:59:55 +0000

    Add class form for posting posts on the site

--- cookingblog/forms.py
+++ cookingblog/forms.py
@@ -1,6 +1,6 @@
-from .models import Comment
+from .models import Comment, CookingRecipePost
 from django import forms
-from .models import CookingRecipePost
+
 
 
 class CommentForm(forms.ModelForm):
@@ -10,3 +10,12 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class CookingRecipePostCreateForm(forms.ModelForm):
+    """
+    Form for posting posts  to the site
+    """
+    class Meta:
+        model = CookingRecipePost
+        fields = ('cooking_recipe_title', 'cat', 'cooking_recipe_image', 'excerpt', 'cooking_recipe_body')

0f7cbd12caae10d919f8913a19769959fd39b143
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-30 06:51:59 +0000

    Create profile model and make migrations to DB

--- customactions/models.py
+++ customactions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Profile Model
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(blank=True, max_length=50)
+    avatar = CloudinaryField('image', default='placeholder')
+    bio = models.TextField(max_length=500, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    region = models.CharField(blank=True, max_length=50)
+    occupation = models.CharField(blank=True, max_length=50)
+
+
+    def __str__(self):
+        return self.user.username

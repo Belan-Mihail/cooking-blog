aed8dc8e0bc0cab53e7bb515b61e691c780bb466
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-30 21:38:32 +0000

    Add click function to display new quotes when has  new quotes button clicked

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -31,3 +31,19 @@ if ($('#quote').length > 0){
         }
     })
 }
+
+$('#quote-btn').click(function() {
+	$.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+        headers: { 'X-Api-Key': 'eNB8L18RTwR2FWqjPxFP9Q==swzK4Ndsww2aGENA'},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result[0].quote);
+            $('#quote').html(result[0].quote);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    })
+});

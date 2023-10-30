0daa8638bd9f1dca9eaefb9814e6025030d74fca
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-30 18:21:02 +0000

    Add view to see detail of user profile

--- customactions/views.py
+++ customactions/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from .forms import ProfileForm
 from .models import Profile
+from cookingblog.models import CookingRecipePost
 
 
 class CreateProfileView(SuccessMessageMixin, CreateView):
@@ -23,3 +24,22 @@ class CreateProfileView(SuccessMessageMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class UserProfilePageView(DetailView):
+    """
+    A class view to see detail
+    of user profile
+    """
+    model = Profile
+    template_name = 'profile_page.html'
+    
+    
+    def get_object(self, *args, **kwargs):
+        return self.request.user
+
+
+    def get_context_data(self,**kwargs):
+        context = super(UserProfilePageView, self).get_context_data(**kwargs)
+        context['cooking_recipes_post'] = CookingRecipePost.objects.filter(status=1).order_by("-created_on")
+        return context

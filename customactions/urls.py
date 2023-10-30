e37ded1c8aacb386557381e8eb33b5c74598c966
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-30 19:51:11 +0000

    Add template to update_page and edit link in create_profile page

--- customactions/urls.py
+++ customactions/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import CreateProfileView, ProfilePageView
+from .views import CreateProfileView, ProfilePageView, UpdateProfile
 
 
 urlpatterns = [
     path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
     path('profile_page/', views.ProfilePageView.as_view(), name='profile_page'),
+    path('update_profile/', UpdateProfile.as_view(), name='update_profile'),
 
 ]

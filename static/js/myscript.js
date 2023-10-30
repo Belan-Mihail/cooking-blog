585166801a98c4c6f3764104e1abb61f938825e8
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-30 21:14:49 +0000

    Add setinterval function and use open Api to display on the site new food qoutes each 30 second

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,1 +1,16 @@
-console.log('Hi')
+var category = 'food'
+setInterval(function () {
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+    headers: { 'X-Api-Key': 'eNB8L18RTwR2FWqjPxFP9Q==swzK4Ndsww2aGENA'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result[0].quote);
+        $('#quote').html(result[0].quote);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+})
+}, 30000);

325686f0173fccafa75730582b09c4d707fc4b78
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-11-03 19:01:40 +0000

    Add test for checking profile model str

--- customactions/tests/test_models.py
+++ customactions/tests/test_models.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 from customactions.models import Profile, Contact
 from django.contrib.auth.models import User
+
+class TestProfileModel(TestCase):
+    """
+    Test for Profile Model
+    """
+    def setUp(self):
+        User.objects.create(username='admin',),
+        self.profile = Profile.objects.create(
+            nickname="administrator",
+            bio='bio',
+            user_id=1,
+            region = "Germany",
+            occupation='developper',
+            )
+
+    def test_profile_str(self):
+        """
+        Checks str for Profile Model
+        """
+        obj = self.profile
+        self.assertEqual(str(obj), "admin")
+    
+
+   
